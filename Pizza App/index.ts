34c719f136a98e9d5ce483a1817826d869681f18
ovezthaking <oliwerx12@gmail.com>
2025-11-18 02:18:47 +0100

    Defensive codding

--- Pizza App/index.ts
+++ Pizza App/index.ts
@@ -15,6 +15,10 @@ function addNewPizza(pizzaObj) {
 
 function placeOrder(pizzaName) {
     const selectedPizza = menu.find(pizzaObj => pizzaObj.name === pizzaName)
+    if (!selectedPizza){
+        console.error(`${pizzaName} does not exist in the menu`);
+        return
+    }
     cashInRegister += selectedPizza.price
     const newOrder = { id: nextOrderId++, pizza: selectedPizza, status: "ordered" }
     orderQueue.push(newOrder)
